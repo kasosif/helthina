5b4ff00dd9b7a87e9277e061977ad34379327cdb
Marwen Chhibi <mchhibi@ecovadis.com>
2021-06-04 03:14:19 +0100

    refactor adresse and web adresse

--- resources/views/dash/gestionAdresse.blade.php
+++ resources/views/dash/gestionAdresse.blade.php
@@ -1,104 +1,121 @@
 @extends('layouts.appDash')
 @section('title') Gestion adresse @endsection
-@section('csspage') @endsection
+@section('csspage')
+<link href="{{asset('backassets/plugins/summernote/summernote.css')}}" rel="stylesheet" />
+@endsection
 @section('content')
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card-box">
+            <h4 class="m-t-0 header-title"><b>Liste des adresses</b></h4>
 
-<h3>Gestion adresse</h3>
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">Nom</th>
-            <th scope="col">Emplacement</th>
-            <th scope="col">Image</th>
-            <th scope="col"></th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($adresses as $adress)
-        <tr>
-            <td>{{$adress->name}} </td>
-            <td>{{$adress->localisation}}</td>
-            <td><img class="img-fluid" data-bss-hover-animate="pulse" src="{{asset('uploads/adresse_images/'.$adress->image)}}" alt="alt text here" style="border: 1px solid rgb(135,135,135) ; height: 100px;"></td>
-            <td>
-                <form method="POST" action="{{route('dash.deleteAdresse',['adresse_id' => $adress->id])}}">
+            @if($adresses->count())
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Nom</th>
+                        <th>Emplacement</th>
+                        <th>Image</th>
+                        <th>Liens</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($adresses as $adress)
+                    <tr>
+                        <td>
+                            <img class="img-fluid" data-bss-hover-animate="pulse" src="{{asset('uploads/adresse_images/'.$adress->image)}}" alt="alt text here" style="border: 1px solid rgb(135,135,135) ; height: 100px;">
+                        </td>
+                        <td>{{$adress->name}} </td>
+                        <td>{{$adress->localisation}}</td>
+                        <td>
+                            <a href="{{$adress->instagram_link}}"><i class="fa fa-instagram" style="font-size: 28px;"></i></a>
+                            <a href="{{$adress->fb_link}}"><i class="fa fa-facebook-official" style="font-size: 28px;"></i></a>
+                            <a href="{{$adress->map_link}}"><i class="fa fa-map-marker localisation" style="font-size: 28px;"></i></a>
+                        </td>
+                        <td class="row">
+                            <form style="display: inline;" method="POST" action="{{route('dash.deleteAdresse',['adresse_id' => $adress->id])}}">
+                                @csrf
+                                @method('delete')
+                                <button type="submit" class="btn btn-danger" role="button">
+                                    <i class="fa fa-trash" style="font-size: 16px;"></i> </button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            @else
+            <div class="alert alert-info">
+                Aucune Adresse trouvé
+            </div>
+            @endif
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card-box">
+            <h4 class="m-t-0 header-title"><b>Ajouter une adresse</b></h4>
+
+            <div class="row" style="margin-top: 15px;">
+                <form role="form" action="{{route('dash.ajouterAdresse')}}" method="post" enctype="multipart/form-data">
                     @csrf
-                    @method('delete')
-                    <button type="submit" class="btn btn-danger signin" role="button">
-                        Supprimer </button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+                    <div class="row" style="margin-bottom: 20px;">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="image1">Image Principale</label>
+                                <input class="filestyle" data-input="false" id="image1" type="file" name="image">
+                            </div>
+                        </div>
+                    </div>
 
-<form method="post" id="add-article" action="{{route('dash.ajouterAdresse')}}" enctype="multipart/form-data">
-    @csrf
-    <div id="booking_fieldset">
-        <div class="block">
-            <div class="block-body">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="form-group">
-                            <label for="title">Nom<span class="text-danger">*</span>
-                            </label>
-                            <input type="text" required name="name" class="form-control" id="title" title="Nom" placeholder="Nom">
-                        </div>
+                    <div class="form-group">
+                        <label for="name">Nom</label>
+                        <input type="text" name="name" required id="name" class="form-control">
                     </div>
-                </div>
-                <div class="row-separator"></div>
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="form-group">
-                            <label for="title">Emplacement<span class="text-danger">*</span>
-                            </label>
-                            <input type="text" required name="localisation" class="form-control" id="title" title="Emplacement" placeholder="Emplacement">
-                        </div>
+                    <div class="form-group">
+                        <label for="localisation">Emplacement</label>
+                        <input type="text" name="localisation" required id="localisation" class="form-control">
                     </div>
-                </div>
-                <div class="row-separator"></div>
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="form-group">
-                            <label for="title">Lien google maps<span class="text-danger">*</span>
-                            </label>
-                            <input type="text" required name="map_link" class="form-control" id="title" title="Lien google maps" placeholder="Lien google maps">
-                        </div>
+                    <div class="form-group">
+                        <label for="map_link">Lien Google maps</label>
+                        <input type="text" name="map_link" required id="map_link" class="form-control">
                     </div>
-                </div>
-                <div class="row-separator"></div>
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="form-group">
-                            <label for="title">Lien page facebook<span class="text-danger">*</span>
-                            </label>
-                            <input type="text" required name="fb_link" class="form-control" id="title" title="Lien page facebook" placeholder="Lien page facebook">
-                        </div>
+                    <div class="form-group">
+                        <label for="fb_link">Lien Facebook</label>
+                        <input type="text" name="fb_link" required id="fb_link" class="form-control">
                     </div>
-                </div>
-                <div class="row-separator"></div>
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="form-group">
-                            <label for="title">Lien page instagram<span class="text-danger">*</span>
-                            </label>
-                            <input type="text" required name="instagram_link" class="form-control" id="title" title="Lien page instagram" placeholder="Lien page instagram">
-                        </div>
+                    <div class="form-group">
+                        <label for="instagram_link">Lien Instagram</label>
+                        <input type="text" name="instagram_link" required id="instagram_link" class="form-control">
                     </div>
-                </div>
-                <input type="file" name="image" onchange="loadFile(event)" id="article_gallery">
-                <div class="row-separator"></div>
-                <div class="row " id="article_img_prev" style="display: none; margin-top: 14px;"></div>
-                <div class="row-separator"></div>
-                <div class="row">
-                    <div class="block-right">
-                        <button type="submit" class="btn btn-success btn-lg">Ajouter Adresse</button>
+                    <div class="form-group">
+                        <button class="btn btn-success btn-block" type="submit">Ajouter Adresse</button>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
-</form><!-- #add-property-form -->
+</div>
+
 
 @endsection
-@section('jspage') @endsection
+@section('jspage')
+<script src="{{asset('backassets/plugins/summernote/summernote.min.js')}}"></script>
+<script src="{{asset('backassets/plugins/bootstrap-filestyle/js/bootstrap-filestyle.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('backassets/plugins/notifyjs/js/notify.js')}}"></script>
+<script src="{{asset('backassets/plugins/notifications/notify-metro.js')}}"></script>
+<script>
+    $('.summernote').summernote({
+        height: 350, // set editor height
+        minHeight: null, // set minimum height of editor
+        maxHeight: null, // set maximum height of editor
+        focus: false // set focus to editable area after initializing summernote
+    });
+    @if(session()->has('success'))
+    $.Notification.notify('success', 'top right', 'Youpi', '{{session()->get('
+        success ')}}');
+    @endif
+</script>
+@endsection
--- resources/views/dash/gestionWebAdresse.blade.php
+++ resources/views/dash/gestionWebAdresse.blade.php
@@ -1,64 +1,97 @@
 @extends('layouts.appDash')
-@section('title') Gestion adresse web @endsection
-@section('csspage') @endsection
+@section('title') Gestion adresse @endsection
+@section('csspage')
+<link href="{{asset('backassets/plugins/summernote/summernote.css')}}" rel="stylesheet" />
+@endsection
 @section('content')
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card-box">
+            <h4 class="m-t-0 header-title"><b>Liste des adresses web</b></h4>
 
-<h3>Gestion adresse web</h3>
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">Lien</th>
-            <th scope="col">Image</th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($webAdresses as $adress)
-        <tr>
-            <td>{{$adress->map_link}} </td>
-            <td><img class="img-fluid" data-bss-hover-animate="pulse" src="{{asset('uploads/web_adresse_images/'.$adress->image)}}" alt="alt text here" style="border: 1px solid rgb(135,135,135) ; height: 100px;"></td>
-            <td>
-                <form method="POST" action="{{route('dash.deleteWebAdresse',['web_address_id' => $adress->id])}}">
+            @if($webAdresses->count())
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Liens</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($webAdresses as $adress)
+                    <tr>
+                        <td>
+                            <img class="img-fluid" data-bss-hover-animate="pulse" src="{{asset('uploads/web_adresse_images/'.$adress->image)}}" alt="alt text here" style="border: 1px solid rgb(135,135,135) ; height: 100px;">
+                        </td>
+                        <td><a href="{{$adress->map_link}}"><i class="ti-world" style="font-size: 28px;"></i></a></td>
+                        <td class="row">
+                            <form style="display: inline;" method="POST" action="{{route('dash.deleteWebAdresse',['web_address_id' => $adress->id])}}">
+                                @csrf
+                                @method('delete')
+                                <button type="submit" class="btn btn-danger" role="button">
+                                    <i class="fa fa-trash" style="font-size: 16px;"></i> </button>
+                            </form>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            @else
+            <div class="alert alert-info">
+                Aucune Adresse web trouvé
+            </div>
+            @endif
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card-box">
+            <h4 class="m-t-0 header-title"><b>Ajouter une adresse web</b></h4>
+
+            <div class="row" style="margin-top: 15px;">
+                <form role="form" action="{{route('dash.ajouterWebAdresse')}}" method="post" enctype="multipart/form-data">
                     @csrf
-                    @method('delete')
-                    <button type="submit" class="btn btn-danger signin" role="button">
-                        Supprimer </button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
-
-<form method="post" id="add-article" action="{{route('dash.ajouterWebAdresse')}}" enctype="multipart/form-data">
-    @csrf
-    <div id="booking_fieldset">
-        <div class="block">
-            <div class="block-body">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="form-group">
-                            <label for="title">Lien<span class="text-danger">*</span>
-                            </label>
-                            <input type="text" required name="map_link" class="form-control" id="title" title="Lien" placeholder="Lien">
+                    <div class="row" style="margin-bottom: 20px;">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="image1">Image</label>
+                                <input class="filestyle" data-input="false" id="image1" type="file" name="image">
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="row-separator"></div>
-                <input type="file" name="image" onchange="loadFile(event)" id="article_gallery">
-                <div class="row-separator"></div>
-                <div class="row " id="article_img_prev" style="display: none; margin-top: 14px;"></div>
-                <div class="row-separator"></div>
-                <div class="row">
-                    <div class="block-right">
-                        <button type="submit" class="btn btn-success btn-lg">Ajouter Adresse Web</button>
+
+                    <div class="form-group">
+                        <label for="lien">Lien</label>
+                        <input type="text" name="map_link" required id="lien" class="form-control">
                     </div>
-                </div>
+                    <div class="form-group">
+                        <button class="btn btn-success btn-block" type="submit">Ajouter Adresse web</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</form><!-- #add-property-form -->
+</div>
+
 
 @endsection
-@section('jspage') @endsection
+@section('jspage')
+<script src="{{asset('backassets/plugins/summernote/summernote.min.js')}}"></script>
+<script src="{{asset('backassets/plugins/bootstrap-filestyle/js/bootstrap-filestyle.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('backassets/plugins/notifyjs/js/notify.js')}}"></script>
+<script src="{{asset('backassets/plugins/notifications/notify-metro.js')}}"></script>
+<script>
+    $('.summernote').summernote({
+        height: 350, // set editor height
+        minHeight: null, // set minimum height of editor
+        maxHeight: null, // set maximum height of editor
+        focus: false // set focus to editable area after initializing summernote
+    });
+    @if(session()->has('success'))
+    $.Notification.notify('success', 'top right', 'Youpi', '{{session()->get('
+        success ')}}');
+    @endif
+</script>
+@endsection
